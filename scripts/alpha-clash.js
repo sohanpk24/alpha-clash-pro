7c0ae34d8df4fc58dddd42d16276bdf0d47f6f11
unknown <sohanpk24@gmail.com>
2024-02-14 22:27:30 +0600

    alphabet color added

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -10,7 +10,23 @@
 //     playgroundSection.classList.remove('hidden');
 // }
 
+function continueGame(){
+    // step-1: generate a random alphabet
+    const alphabet = getRandomAlphabet();
+    console.log('your random alphabet:', alphabet)
+
+    //set randomly generated alphabet to the screen
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+
+    // set background color
+    setBackgroundColorById(alphabet);
+
+}
+
+
 function play(){
     hideElementById('home-screen');
     showElementById('play-ground');
+    continueGame()
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -7,3 +7,25 @@ function showElementById(elementId){
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
 }
+
+function setBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-400');
+
+}
+
+function getRandomAlphabet(){
+    // get or create an alphabet array
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+    // console.log(alphabets)
+
+    // get a random index between 0-25
+    const randomNumber = Math.random()*25;
+    const index = Math.round(randomNumber);
+
+
+    const alphabet = alphabets[index]
+    // console.log(index, alphabet);
+    return alphabet;
+}
