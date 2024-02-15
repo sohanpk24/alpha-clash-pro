b2d73a81c1c9c33b980f23da101eae4ecdcc3c09
unknown <sohanpk24@gmail.com>
2024-02-15 11:54:25 +0600

    added the score and life function

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -10,6 +10,55 @@
 //     playgroundSection.classList.remove('hidden');
 // }
 
+
+function handleKeyboardKeyUpEvent(event){
+    const playerPressed = event.key;
+    console.log('player pressed',playerPressed);
+
+    // get the expected to press
+    const  currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    console.log(playerPressed, expectedAlphabet);
+
+    //check matched or not 
+    if(playerPressed === expectedAlphabet){
+        console.log('you get a point');
+        console.log('you have pressed correctly', expectedAlphabet);
+
+        //update score: step-1 get the current score
+        const currentScoreElement = document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText)
+        console.log(currentScore)
+
+        // step-2 increase the score
+        const newScore = currentScore + 1;
+
+        // step-3 show the updated score
+        currentScoreElement.innerText = newScore;
+
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame()
+    }else{
+        console.log('you missed. you lost the life');
+        //step-1: get the current life number;
+        const currentLifeElement = document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText)
+
+        //step-2: reduce the life count;
+        const newLife = currentLife -1;
+
+        //step-3: display the update life count
+        currentLifeElement.innerText = newLife
+    }
+}
+//capture keyboard key press
+document.addEventListener('keyup', handleKeyboardKeyUpEvent)
+
+
+
 function continueGame(){
     // step-1: generate a random alphabet
     const alphabet = getRandomAlphabet();
--- scripts/utility.js
+++ scripts/utility.js
@@ -14,18 +14,36 @@ function setBackgroundColorById(elementId){
 
 }
 
+
+function removeBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
+
 function getRandomAlphabet(){
-    // get or create an alphabet array
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
     const alphabets = alphabetString.split('');
-    // console.log(alphabets)
 
-    // get a random index between 0-25
     const randomNumber = Math.random()*25;
     const index = Math.round(randomNumber);
 
-
-    const alphabet = alphabets[index]
-    // console.log(index, alphabet);
+    const alphabet = alphabets[index];
     return alphabet;
 }
+
+
+// function getRandomAlphabet(){
+//     // get or create an alphabet array
+//     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+//     const alphabets = alphabetString.split('');
+//     // console.log(alphabets)
+
+//     // get a random index between 0-25
+//     const randomNumber = Math.random()*25;
+//     const index = Math.round(randomNumber);
+
+
+//     const alphabet = alphabets[index]
+//     // console.log(index, alphabet);
+//     return alphabet;
+// }
